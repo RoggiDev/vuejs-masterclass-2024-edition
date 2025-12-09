cf576d8facff55ff0bd495628e019b6862e00b2c
RoggiDev <yael.rodriguez.g@outlook.es>
2025-12-09 00:07:40 -0600

    prepare the vue app with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,18 @@ console.log(supabase)
 </script>
 
 <template>
+  <nav class="p-4 d-flex align-items-center justify-content-between px-4 border-bottom">
+    <!-- Search -->
+    <form class="flex-grow-1" style="max-width: 24rem">
+      <div class="position-relative">
+        <input type="text" class="form-control" placeholder="Search ..." />
+      </div>
+    </form>
+
+    <!-- Avatar -->
+    <div class="rounded-circle bg-white border ms-3" style="width: 2rem; height: 2rem"></div>
+  </nav>
+
   <main>
     <RouterView />
   </main>
