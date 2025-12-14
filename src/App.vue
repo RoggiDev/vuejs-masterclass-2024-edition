e9dc664dcab0b45ad97e0f257a23e7478ec53d81
RoggiDev <yael.rodriguez.g@outlook.es>
2025-12-14 15:27:53 -0600

    use lucide icons with iconify and vue.js

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,24 @@ import Input from '@/components/ui/input/Input.vue'
     <!-- Search -->
     <form class="flex-grow-1" style="max-width: 24rem">
       <div class="position-relative">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="m21 21l-4.34-4.34" />
+            <circle cx="11" cy="11" r="8" />
+          </g>
+        </svg>
+
         <Input type="text" class="form-control" placeholder="Search ..." />
       </div>
     </form>
 
-    <!-- Dropdown Bootstrap -->
+    <!-- Dropdown -->
     <div class="dropdown ms-3">
       <a
         class="d-flex align-items-center text-decoration-none"
@@ -19,7 +32,7 @@ import Input from '@/components/ui/input/Input.vue'
         role="button"
         data-bs-toggle="dropdown"
       >
-        <!-- Avatar Bootstrap -->
+        <!-- Avatar -->
         <img
           src="https://avatars.githubusercontent.com/u/6128107?s=200&v=4"
           alt="@radix-vue"
