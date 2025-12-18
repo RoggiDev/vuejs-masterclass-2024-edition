5e6cd3739d03a640bf7d17d2afd3412cf16b6db2
RoggiDev <yael.rodriguez.g@outlook.es>
2025-12-17 23:06:02 -0600

    configure unplugin-auto-import for unplugin-vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -18,7 +19,7 @@ export default defineConfig({
         /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
         /\.md$/, // .md
       ],
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', VueRouterAutoImports],
       dts: true,
       viteOptimizeDeps: true,
     }),
