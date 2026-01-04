bce9b68838636aaaf4ca0a58f90bbe2e4930a32c
RoggiDev <yael.rodriguez.g@outlook.es>
2026-01-04 14:01:17 -0600

    fetch the user profile and update the auth store

--- src/utils/supaAuth.ts
+++ src/utils/supaAuth.ts
@@ -23,7 +23,8 @@ export const register = async (formData: RegisterForm) => {
     if (error) return console.log('Profiles err:', error)
   }
 
-  authStore.setAuth(data.session)
+  await authStore.setAuth(data.session)
+
   return true
 }
 
@@ -35,7 +36,7 @@ export const login = async (formData: LoginForm) => {
 
   if (error) return console.log(error)
 
-  authStore.setAuth(data.session)
+  await authStore.setAuth(data.session)
 
   return true
 }
